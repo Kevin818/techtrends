d21a069227e1eb1332f649171c30f2b61a0d5a50
Sun <Xiaohua.Sun@philips.com>
2022-09-03 17:53:58 +0800

    20220903001

--- techtrends/app.py
+++ techtrends/app.py
@@ -100,7 +100,7 @@ def create():
 
 # start the application on port 3111
 if __name__ == "__main__":
-        ## stream logs 
+        ## stream logs to 
    logging.basicConfig(level=logging.DEBUG)
 
    app.run(host='0.0.0.0', port='3111')
